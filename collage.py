95c340fef542cb398becde682b5a6b38999e8ffb
Guido Schmidt <git@guidoschmidt.cc>
2018-12-10 22:44:16 +0100

    EDIT/collage.py: use poisson samples for position

--- collage.py
+++ collage.py
@@ -1,6 +1,8 @@
 import bpy
 import os
 import poisson
+import numpy as np
+import math
 
 obj_path = os.path.join(".", "App/obj")
 
@@ -12,14 +14,26 @@ def import_models(obj_path):
                                  axis_forward='-Z',
                                  axis_up='Y',
                                  filter_glob="*.obj")
-                                 
+
+
 objects = bpy.context.visible_objects
-for object in objects:
+objects = list(filter(lambda o: "hsl" in o.name, objects))
+print("# object: {}".format(len(objects)))
+samples = poisson.poisson_disc_samples(width=len(objects) / 10,
+                                       height=len(objects) / 10,
+                                       r=12 / 10)
+print("# samples: {}".format(len(samples)))
+samples_x = list(map(lambda s: s[0], samples))
+samples_y = list(map(lambda s: s[1], samples))
+max_x = np.max(samples_x)
+max_y = np.max(samples_y)
+for i, object in enumerate(objects):
+    dir(object)
+    sample = samples[i]
     object.scale.x = 0.1
     object.scale.y = 0.1
     object.scale.z = 0.1
-
-bpy.context.visible_objects[0].location.x = 0
-bpy.context.visible_objects[0].location.y = 0
-                             
-#print(poisson_disc_samples(width=100, height=100, r=10))
+    object.location.x = sample[0] / 2 - max_x / 4
+    object.location.y = sample[1] / 2 - max_y / 4
+    
+    
